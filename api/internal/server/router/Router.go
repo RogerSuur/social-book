f89426ce4f935b7301df19e1e7d2a27df31c677d
jrms <jarmo.seljamaa@gmail.com>
2023-04-25 22:57:59 +0300

    Implemented Gorilla/Mux router

---
+++ api/internal/server/router/Router.go
@@ -0,0 +1,21 @@
+package router
+
+import (
+	"SocialNetworkRestApi/api/internal/server/handlers"
+
+	"github.com/gorilla/mux"
+)
+
+func New(app *handlers.Application) *mux.Router {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", app.UserService.Authenticate(app.Home)).Methods("GET")
+	r.HandleFunc("/login", app.Login).Methods("POST", "OPTIONS")
+	r.HandleFunc("/signup", app.Register).Methods("POST", "OPTIONS")
+	//r.HandleFunc("/logout", app.UserService.Authenticate(app.Logout))
+	r.HandleFunc("/profile", app.UserService.Authenticate(app.Profile)).Methods("GET")
+	r.HandleFunc("/following", app.UserService.Authenticate(app.Following)).Methods("GET")
+	r.HandleFunc("/followers", app.UserService.Authenticate(app.Followers)).Methods("GET")
+
+	return r
+}
