6be7a299937570d972ccb3b01d8243d8a69f8b03
KeitiN <keiti.nurmbek@gmail.com>
2023-06-03 16:48:30 +0000

    new event methods

--- api/pkg/models/event.go
+++ api/pkg/models/event.go
@@ -37,22 +37,65 @@ func NewEventRepo(db *sql.DB) *EventRepository {
 }
 
 func (repo EventRepository) Insert(event *Event) (int64, error) {
+	query := `INSERT INTO group_events (group_id, user_id, created_at, event_time, timespan, title, description)
+	VALUES(?, ?, ?, ?, ?, ?, ?)`
 
-	//TODO
-	//insert new event into database
-	return 0, nil
+	args := []interface{}{
+		event.GroupId,
+		event.UserId,
+		time.Now(),
+		event.EventTime,
+		event.TimeSpan,
+		event.Title,
+		event.Description,
+	}
+
+	result, err := repo.DB.Exec(query, args...)
+
+	if err != nil {
+		return 0, err
+	}
+
+	lastId, err := result.LastInsertId()
+
+	if err != nil {
+		return 0, err
+	}
+
+	repo.Logger.Printf("Last inserted event '%s' by user %d (last insert ID: %d)", event.Title, event.UserId, lastId)
+
+	return lastId, nil
 }
 
-func (repo EventRepository) GetAllByGroupId(groupId int) ([]*Event, error) {
+func (repo EventRepository) GetAllByGroupId(id int) ([]*Event, error) {
 
-	//TODO
-	//Get all events by group
-	return nil, nil
+	query := `SELECT id, group_id, user_id, created_at, event_time, timespan, title, description FROM group_events WHERE group_id = ?`
+
+	rows, err := repo.DB.Query(query, id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	events := []*Event{}
+
+	for rows.Next() {
+		event := &Event{}
+
+		err := rows.Scan(&event.Id, &event.GroupId, &event.UserId, &event.CreatedAt, &event.EventTime, &event.TimeSpan, &event.TimeSpan, &event.Title, &event.Description)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	repo.Logger.Printf("Found %d events for group %d", len(events), id)
+
+	return events, err
 }
 
 func (repo EventRepository) GetAllByUserId(userId int) ([]*Event, error) {
-
 	//TODO
 	//Get all events by attending user
+
 	return nil, nil
 }
