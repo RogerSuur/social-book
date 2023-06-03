ae097c6d31404025aa96818fec7075899731543f
KeitiN <keiti.nurmbek@gmail.com>
2023-06-03 16:34:16 +0000

    add group_event_attendance model

---
+++ api/pkg/models/groupEventAttendance.go
@@ -0,0 +1,39 @@
+package models
+
+import (
+	"database/sql"
+)
+
+type GroupEventAttendance struct {
+	UserId int
+	PostId int
+}
+
+type GroupEventAttendanceModel struct {
+	DB *sql.DB
+}
+
+func (repo GroupEventAttendanceModel) Insert(attendance *GroupEventAttendance) (int64, error) {
+	query := `INSERT INTO group_event_attendance (user_id, event_id, is_attending)
+	VALUES(?, ?, ?)`
+
+	args := []interface{}{
+		attendance.PostId,
+		attendance.UserId,
+		false,
+	}
+
+	result, err := repo.DB.Exec(query, args...)
+
+	if err != nil {
+		return 0, err
+	}
+
+	lastId, err := result.LastInsertId()
+
+	if err != nil {
+		return 0, err
+	}
+
+	return lastId, nil
+}
