4c31fba0233a2d7ecf41088b4331931e132e60c8
Jollyroger <roger.suuressaar@gmail.com>
2023-07-04 16:46:10 +0300

    searchBar

--- frontend/src/components/GroupSidebar.js
+++ frontend/src/components/GroupSidebar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import GenericGroupList from "../components/GenericGroupList";
 import GenericEventList from "../components/GenericEventList";
+import SearchBar from "./SearchBar";
 import {
   USER_CREATED_GROUPS_URL,
   USER_GROUPS_URL,
@@ -10,20 +11,22 @@ import {
 const GroupSidebar = () => {
   const sidebarItems = (
     <>
-    <ul className="group-sidebar">
-      <h1>Groups</h1>
-      <li>
-        <GenericGroupList url={USER_GROUPS_URL} />
-      </li>
-      <h1>My groups</h1>
-      <li>
-        <GenericGroupList url={USER_CREATED_GROUPS_URL} />
-      </li>
-      <h1>Events</h1>
-      <li>
-        <GenericEventList url={ACCEPTED_EVENTS_URL} />
-      </li>
-     </ul>
+      <h1>Search</h1>
+      <SearchBar />
+      <ul className="group-sidebar">
+        <h1>Groups</h1>
+        <li>
+          <GenericGroupList url={USER_GROUPS_URL} />
+        </li>
+        <h1>My groups</h1>
+        <li>
+          <GenericGroupList url={USER_CREATED_GROUPS_URL} />
+        </li>
+        <h1>Events</h1>
+        <li>
+          <GenericEventList url={ACCEPTED_EVENTS_URL} />
+        </li>
+      </ul>
     </>
   );
 
---
+++ frontend/src/components/SearchBar.js
@@ -0,0 +1,80 @@
+import { useEffect, useState, setError } from "react";
+import { makeRequest } from "../services/makeRequest.js";
+
+const SearchBar = () => {
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchString, setSearchString] = useState("");
+
+  //   useEffect(() => {
+  //     const abortController = new AbortController();
+  //     const loadPosts = async () => {
+  //       try {
+  //         console.log(searchString);
+  //         // const response = await makeRequest(`search/${searchString}`, {
+  //         //   signal: abortController.signal,
+  //         // });
+  //         fetch("https://jsonplaceholder.typicode.com/users")
+  //           .then((response) => response.json())
+  //           .then((response) => {
+  //             console.log(response);
+  //             setSearchResults(response);
+  //           });
+  //       } catch (error) {
+  //         setError(error.message);
+  //       }
+  //     };
+  //     loadPosts();
+
+  //     console.log(searchResults.userName);
+
+  //     return () => {
+  //       abortController.abort();
+  //     };
+  //   }, [searchString]);
+
+  const fetchData = () => {
+    try {
+      fetch("https://jsonplaceholder.typicode.com/users")
+        .then((response) => response.json())
+        .then((response) => {
+          console.log(response);
+          setSearchResults(response);
+        });
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  const handleChange = (e) => {
+    if (!e.target.value) {
+      setSearchResults([]);
+    }
+
+    setSearchString(e.target.value);
+    fetchData(searchString);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <form id="form" onSubmit={handleSubmit} onChange={handleChange}>
+        <input
+          className="search"
+          type="text"
+          id="search"
+          placeholder="Search.."
+          onChange={handleChange}
+        />
+        {/* <button className="search-button">Search</button> */}
+      </form>
+      {searchResults.map((result) => (
+        <div key={result.id}>{result.name}</div>
+      ))}
+    </>
+  );
+};
+
+export default SearchBar;
