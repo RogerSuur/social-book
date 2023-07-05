ccd84f00a4d918503893369b386415339326b22c
Jollyroger <roger.suuressaar@gmail.com>
2023-07-05 17:21:04 +0300

    auth error fix

--- frontend/src/components/SearchResults.js
+++ frontend/src/components/SearchResults.js
@@ -1,21 +1,22 @@
+import { Link } from "react-router-dom";
 export const SearchResults = ({ searchResults }) => {
+  const searchResultsMap = searchResults.map((result, index) => (
+    <div key={index}>
+      <Link
+        to={
+          result.userId === 0
+            ? `/groups/${result.groupId}`
+            : `/profile/${result.userId}`
+        }
+      >
+        {result.name}
+      </Link>
+    </div>
+  ));
+
   return (
     <div className="results-list">
-      {searchResults &&
-        searchResults.map((result) => (
-          <div>
-            <a
-              key={result.id}
-              href={
-                result.userId === 0
-                  ? `/groups/${result.groupId}`
-                  : `profile/${result.userId}`
-              }
-            >
-              {result.name}
-            </a>
-          </div>
-        ))}
+      <ul>{searchResultsMap}</ul>
     </div>
   );
 };
