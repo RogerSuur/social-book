83874accd4303d2cc9734f35d74c05ce54b4746e
Jollyroger <roger.suuressaar@gmail.com>
2023-07-05 18:41:15 +0300

    groupmembers

--- frontend/src/components/GroupMembers.js
+++ frontend/src/components/GroupMembers.js
@@ -8,16 +8,28 @@ const GroupMembers = ({ groupId }) => {
   const [modalOpen, setModalOpen] = useState(false);
 
   useEffect(() => {
+    console.log("groupMembers", `/groupmembers/${groupId}`);
     const loadMembers = async () => {
       try {
         const response = await makeRequest(`/groupmembers/${groupId}`);
-        setGroupMembers(response);
+        if (response !== null) {
+          setGroupMembers(response);
+        }
+        console.log("groupmembers response", response);
       } catch (error) {
         setError(error.message);
       }
     };
     loadMembers();
   }, []);
+
+  const groupMembersMap = groupMembers.map((member, index) => (
+    <div key={index}>
+      <p>User ID: {member.userId}</p>
+      <p>Username: {member.userName}</p>
+      <p>Image Path: {member.imagePath}</p>
+    </div>
+  ));
 
   const openModal = () => {
     setModalOpen(true);
@@ -29,19 +41,20 @@ const GroupMembers = ({ groupId }) => {
 
   return (
     <>
-      <p>
-        This group with id: {groupId} has {groupMembers.length} members
-      </p>
-      <button onClick={openModal}>Show Members</button>
-      <Modal open={modalOpen} onClose={closeModal}>
-        {groupMembers.map((member) => (
-          <div key={member.userId}>
-            <p>User ID: {member.userId}</p>
-            <p>Username: {member.userName}</p>
-            <p>Image Path: {member.imagePath}</p>
-          </div>
-        ))}
-      </Modal>
+      {groupMembers.length > 0 ? (
+        <>
+          <p>
+            This group with id: {groupId} has {groupMembers.length} members
+          </p>
+          <button onClick={openModal}>Show Members</button>
+          <Modal open={modalOpen} onClose={closeModal}>
+            {groupMembersMap}
+          </Modal>
+        </>
+      ) : (
+        <p>Apply to become a member of this group</p>
+      )}
+      ;
     </>
   );
 };
