b38ae28018d88d37bc2b99b75cf5bdab6d82a366
inquisitivetable <88971685+inquisitivetable@users.noreply.github.com>
2023-07-26 18:47:44 +0300

    event page with modal

--- frontend/src/pages/EventPage.js
+++ frontend/src/pages/EventPage.js
@@ -3,10 +3,15 @@ import { useParams, useNavigate, Link } from "react-router-dom";
 import { EVENT_URL } from "../utils/routes";
 import axios from "axios";
 import ImageHandler from "../utils/imageHandler";
+import GroupSidebar from "../components/GroupSidebar";
+import Modal from "../components/Modal.js";
+import { set } from "react-hook-form";
 
 const EventPage = () => {
   const [event, setEvent] = useState({});
   const [error, setError] = useState("");
+  const [modalOpen, setModalOpen] = useState(false);
+  const [activeTab, setActiveTab] = useState(true);
   const navigate = useNavigate();
   const { id } = useParams();
 
@@ -34,24 +39,65 @@ const EventPage = () => {
     loadEvent();
   }, [id]);
 
-  //   const groupMembersMap = groupMembers.map((member, index) => (
-  //     <div key={index}>
-  //       <Link to={`/profile/${member.id}`}>
-  //         {ImageHandler(member.imagePath, "defaultuser.jpg", "profile-image")}
-  //         <p>{member.nickname}</p>
-  //       </Link>
-  //     </div>
-  //   ));
+  console.log("EVENT DATA: ", event);
+
+  const userList = (attendance) => {
+    const users = event?.members?.filter(
+      (member) => member.isAttending === attendance
+    );
+
+    return users?.map((member, index) => (
+      <li key={index}>
+        <Link to={`/profile/${member.userId}`}>
+          {ImageHandler(member.imagePath, "defaultuser.jpg", "profile-image")}
+          <p>{member.name}</p>
+        </Link>
+      </li>
+    ));
+  };
+
+  const timeConverter = (datetime) =>
+    new Date(datetime).toLocaleTimeString("en-UK", {
+      month: "short",
+      day: "2-digit",
+      year: "2-digit",
+      hour: "numeric",
+      minute: "2-digit",
+    });
+
+  const handleModalClose = () => {
+    setModalOpen(false);
+  };
+
+  const handleModalClick = () => {
+    setModalOpen(true);
+  };
+
+  console.log("ACTIVE", activeTab);
 
   const renderedEvent = (
     <div>
-      <p>{event?.name}</p>
-      <p>{event?.body}</p>
-      <p>{new Date(event?.datetime).toLocaleString("et-EE")}</p>
+      <p>{event?.title}</p>
+      <p>{event?.description}</p>
+      <p>Start: {timeConverter(event?.eventTime)}</p>
+      <p>End: {timeConverter(event?.eventEndTime)}</p>
+      <Modal open={modalOpen} onClose={handleModalClose}>
+        <ul>
+          <li onClick={() => setActiveTab(true)}>Going</li>
+          <li onClick={() => setActiveTab(false)}>Not Going</li>
+        </ul>
+        <ul>{userList(activeTab)}</ul>
+      </Modal>
+      <button onClick={handleModalClick}>See who is going</button>
     </div>
   );
 
-  return <>{error ? <div>{error}</div> : <div>{renderedEvent}</div>}</>;
+  return (
+    <>
+      <GroupSidebar />
+      {error ? <div>{error}</div> : <div>{renderedEvent}</div>}
+    </>
+  );
 };
 
 export default EventPage;
