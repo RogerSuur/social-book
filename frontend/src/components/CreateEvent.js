7bc2867010d67deb5e3594224393c9b5524a857f
Jollyroger <roger.suuressaar@gmail.com>
2023-09-17 19:01:14 +0300

    creating events

--- frontend/src/components/CreateEvent.js
+++ frontend/src/components/CreateEvent.js
@@ -1,122 +1,128 @@
-import Modal from "./Modal";
 import { useState } from "react";
 import axios from "axios";
 
+import { Container, Form, Button, FloatingLabel, Alert } from "react-bootstrap";
+
 const CreateEvent = ({ onEventCreated, id }) => {
-  const [modalOpen, setModalOpen] = useState(false);
-  const [errMsg, setErrMsg] = useState("");
-  const [createEventForm, setCreateEventForm] = useState({
-    title: "",
-    description: "",
-    startTime: "",
-    endTime: "",
-    group_id: +id,
-  });
+    const [errMsg, setErrMsg] = useState("");
+    const [createEventForm, setCreateEventForm] = useState({
+        title: "",
+        description: "",
+        startTime: "",
+        endTime: "",
+        group_id: +id,
+    });
 
-  const openModal = () => {
-    setModalOpen(true);
-  };
+    const handleChange = (event) => {
+        setErrMsg("");
+        const { name, value } = event?.target;
+        setCreateEventForm((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
 
-  const closeModal = () => {
-    setModalOpen(false);
-  };
+    const handleSubmit = async (event) => {
+        event.preventDefault();
 
-  const handleChange = (event) => {
-    const { name, value } = event?.target;
-    setCreateEventForm((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    // Send the createEventForm data to the backend handler
-    try {
-      const response = await axios.post(
-        "http://localhost:8000/creategroupevent",
-        JSON.stringify({
-          ...createEventForm,
-          startTime: new Date(createEventForm.startTime).toISOString(),
-          endTime: new Date(createEventForm.endTime).toISOString(),
-        }),
-        { withCredentials: true },
-        {
-          headers: { "Content-Type": "application/json" },
+        if (
+            createEventForm?.title === "" ||
+            createEventForm?.description === "" ||
+            createEventForm?.startTime === ""
+        ) {
+            setErrMsg("Some fields missing");
+            return;
         }
-      );
-      setErrMsg(response.data?.message);
-      onEventCreated();
-    } catch (err) {
-      if (!err?.response) {
-        setErrMsg("No Server Response");
-      } else {
-        setErrMsg("Internal Server Error");
-      }
-    }
+        // Send the createEventForm data to the backend handler
+        try {
+            const response = await axios.post(
+                "http://localhost:8000/creategroupevent",
+                JSON.stringify({
+                    ...createEventForm,
+                    startTime: new Date(
+                        createEventForm.startTime
+                    ).toISOString(),
+                    endTime: new Date(createEventForm.endTime).toISOString(),
+                }),
+                { withCredentials: true },
+                {
+                    headers: { "Content-Type": "application/json" },
+                }
+            );
 
-    closeModal();
-  };
+            if (response.status === 200) {
+                setErrMsg("New Event Created");
+                onEventCreated();
+            } else {
+                setErrMsg("Server responded with an unexpected status code");
+            }
+        } catch (err) {}
+    };
 
-  return (
-    <>
-      <div className="newModal">
-        <i className="iconoir-add-circle" onClick={openModal} />
-        <Modal open={modalOpen} onClose={closeModal}>
-          {errMsg && <p className="error">{errMsg}</p>}
-          <form className="pop-form" onSubmit={handleSubmit}>
-            Title:
-            <label className="input-big">
-              <input
-                type="text"
-                name="title"
-                value={createEventForm.title}
-                onChange={handleChange}
-                required
-              />
-            </label>
-            <br />
-            Description:
-            <label>
-              <textarea
-                className="text-big"
-                name="description"
-                value={createEventForm.description}
-                onChange={handleChange}
-                required
-              ></textarea>
-            </label>
-            <br />
-            Start Time:
-            <label>
-              <input
-                type="datetime-local"
-                name="startTime"
-                value={createEventForm.startTime}
-                onChange={handleChange}
-                required
-              />
-            </label>
-            <br />
-            End Time:
-            <label>
-              <input
-                type="datetime-local"
-                name="endTime"
-                value={createEventForm.endTime}
-                onChange={handleChange}
-                required
-              />
-            </label>
-            <br />
-            <button className="create-but" type="submit">
-              Create
-            </button>
-          </form>
-        </Modal>
-      </div>
-    </>
-  );
+    return (
+        <>
+            {errMsg && (
+                <Alert
+                    variant={
+                        errMsg === "New Event Created" ? "success" : "danger"
+                    }
+                    className="text-center"
+                >
+                    {errMsg}
+                </Alert>
+            )}
+            <Form onSubmit={handleSubmit}>
+                {errMsg && <p className="error">{errMsg}</p>}
+                <form className="pop-form">
+                    Title:
+                    <label className="input-big">
+                        <input
+                            type="text"
+                            name="title"
+                            value={createEventForm.title}
+                            onChange={handleChange}
+                            required
+                        />
+                    </label>
+                    <br />
+                    Description:
+                    <label>
+                        <textarea
+                            className="text-big"
+                            name="description"
+                            value={createEventForm.description}
+                            onChange={handleChange}
+                            required
+                        ></textarea>
+                    </label>
+                    <br />
+                    Start Time:
+                    <label>
+                        <input
+                            type="datetime-local"
+                            name="startTime"
+                            value={createEventForm.startTime}
+                            onChange={handleChange}
+                            required
+                        />
+                    </label>
+                    <br />
+                    End Time:
+                    <label>
+                        <input
+                            type="datetime-local"
+                            name="endTime"
+                            value={createEventForm.endTime}
+                            onChange={handleChange}
+                            required
+                        />
+                    </label>
+                    <br />
+                </form>
+                <Button type="submit">Create</Button>
+            </Form>
+        </>
+    );
 };
 
 export default CreateEvent;
--- frontend/src/pages/GroupPage.js
+++ frontend/src/pages/GroupPage.js
@@ -12,98 +12,121 @@ import CreateGroupPosts from "../components/CreateGroupPosts.js";
 import GenericModal from "../components/GenericModal";
 import AddGroupMembers from "../components/AddGroupMembers";
 import { Alert, Container, Stack, Col, Row, Badge } from "react-bootstrap";
-import { PlusCircle } from "react-bootstrap-icons";
+import { Calendar2Check, PlusCircle } from "react-bootstrap-icons";
+import CreateEvent from "../components/CreateEvent";
 
 const GroupPage = () => {
-  const [group, setGroup] = useState({});
-  const { id } = useParams();
-  const [errMsg, setErrMsg] = useState(null);
-  const [reload, setReload] = useState(false);
+    const [group, setGroup] = useState({});
+    const { id } = useParams();
+    const [errMsg, setErrMsg] = useState(null);
+    const [reload, setReload] = useState(false);
 
-  const handlePostUpdate = () => {
-    setReload(!reload);
-  };
+    const handlePostUpdate = () => {
+        setReload(!reload);
+    };
 
-  const loadGroup = async () => {
-    try {
-      await axios
-        .get(GROUP_PAGE_URL + id, {
-          withCredentials: true,
-        })
-        .then((response) => {
-          console.log("RESP: ", response.data);
-          setGroup(response.data);
-        });
-    } catch (err) {
-      setErrMsg(err.message);
-    }
-  };
+    const loadGroup = async () => {
+        try {
+            await axios
+                .get(GROUP_PAGE_URL + id, {
+                    withCredentials: true,
+                })
+                .then((response) => {
+                    console.log("RESP: ", response.data);
+                    setGroup(response.data);
+                });
+        } catch (err) {
+            setErrMsg(err.message);
+        }
+    };
 
-  const handleAvatarUpdate = () => {
-    loadGroup();
-  };
+    const handleAvatarUpdate = () => {
+        loadGroup();
+    };
 
-  useEffect(() => {
-    loadGroup();
-  }, [id]);
+    useEffect(() => {
+        loadGroup();
+    }, [id]);
 
-  const image = ImageHandler(group.imagePath, "defaultgroup.png", "group-img");
+    const image = ImageHandler(
+        group.imagePath,
+        "defaultgroup.png",
+        "group-img"
+    );
 
-  return (
-    <>
-      {/* <div style={{ "max-width": `100px` }}>
+    return (
+        <>
+            {/* <div style={{ "max-width": `100px` }}>
         <Events groupId={+id} />
       </div> */}
-      {errMsg ? (
-        <Alert variant="danger" className="text-center">
-          {errMsg}
-        </Alert>
-      ) : (
-        <Container fluid>
-          {image}
-          <Stack direction="horizontal" gap="2">
-            <Col xs="auto">
-              <h1>{group.title}</h1>
-            </Col>
-            <Col xs="auto">
-              {group?.isMember ? (
-                <Stack direction="horizontal">
-                  <div>
-                    <GroupMembers groupId={+id} />
-                  </div>
-                  <div>
-                    <GenericModal
-                      img={<PlusCircle />}
-                      variant="flush"
-                      headerText="Add members"
-                    >
-                      <AddGroupMembers id={+id} />
-                    </GenericModal>
-                  </div>
-                </Stack>
-              ) : (
-                <GroupRequestButton groupid={+id} />
-              )}
-            </Col>
-          </Stack>
+            {errMsg ? (
+                <Alert variant="danger" className="text-center">
+                    {errMsg}
+                </Alert>
+            ) : (
+                <Container fluid>
+                    {image}
+                    <Stack direction="horizontal" gap="2">
+                        <Col xs="auto">
+                            <h1>{group.title}</h1>
+                        </Col>
+                        <Col xs="auto">
+                            {group?.isMember ? (
+                                <Stack direction="horizontal">
+                                    <div>
+                                        <GroupMembers groupId={+id} />
+                                    </div>
+                                    <div>
+                                        <GenericModal
+                                            img={<PlusCircle />}
+                                            variant="flush"
+                                            headerText="Add members"
+                                        >
+                                            <AddGroupMembers id={+id} />
+                                        </GenericModal>
+                                    </div>
 
-          <p>{group.description}</p>
-          {group.isMember && (
-            <>
-              {/* <GenericModal buttonText="Upload new image">
+                                    <div>
+                                        <GenericModal
+                                            img={<Calendar2Check />}
+                                            variant="flush"
+                                            headerText="New event"
+                                        >
+                                            {/* <AddGroupMembers id={+id} /> */}
+                                            <CreateEvent id={+id} />
+                                        </GenericModal>
+                                    </div>
+                                </Stack>
+                            ) : (
+                                <GroupRequestButton groupid={+id} />
+                            )}
+                        </Col>
+                    </Stack>
+
+                    <p>{group.description}</p>
+                    {group.isMember && (
+                        <>
+                            {/* <GenericModal buttonText="Upload new image">
                 <AvatarUpdater
                   url={`${GROUP_PAGE_URL}${id}/avatar`}
                   onUploadSuccess={handleAvatarUpdate}
                 />
               </GenericModal> */}
-              <CreateGroupPosts groupId={id} onPostsUpdate={handlePostUpdate} />
-              <FeedPosts url={GROUPFEED_URL + id} key={id} reload={reload} />
-            </>
-          )}
-        </Container>
-      )}
-    </>
-  );
+                            <CreateGroupPosts
+                                groupId={id}
+                                onPostsUpdate={handlePostUpdate}
+                            />
+                            <FeedPosts
+                                url={GROUPFEED_URL + id}
+                                key={id}
+                                reload={reload}
+                            />
+                        </>
+                    )}
+                </Container>
+            )}
+        </>
+    );
 };
 
 export default GroupPage;
