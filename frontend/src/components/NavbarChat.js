9d2c34a83deef829982f1b84c890c0e3275cb26c
inquisitivetable <88971685+inquisitivetable@users.noreply.github.com>
2023-09-26 21:08:34 +0300

    chat notifications

--- frontend/src/components/NavbarChat.js
+++ frontend/src/components/NavbarChat.js
@@ -26,7 +26,7 @@ const NavbarChat = () => {
     <Chat newMessages={newMessages} setNewMessages={setNewMessages} />
   );
 
-  const colour = newMessages && !show && "red";
+  const colour = newMessages && "red";
 
   return (
     <>
