896cebe503612de38b2bb63acab3577248a153e3
Jollyroger <roger.suuressaar@gmail.com>
2023-09-26 20:00:13 +0300

    subprivate public

--- frontend/src/components/CreatePost.js
+++ frontend/src/components/CreatePost.js
@@ -3,15 +3,15 @@ import axios from "axios";
 import Select from "react-select";
 import ImageUploadModal from "./ImageUploadModal";
 import {
-  Form,
-  Image,
-  InputGroup,
-  Alert,
-  Button,
-  Container,
-  Col,
-  Stack,
-  Badge,
+    Form,
+    Image,
+    InputGroup,
+    Alert,
+    Button,
+    Container,
+    Col,
+    Stack,
+    Badge,
 } from "react-bootstrap";
 import PostButton from "../components/PostButton.js";
 import { FOLLOWERS_URL } from "../utils/routes";
@@ -19,204 +19,206 @@ import GenericModal from "../components/GenericModal";
 import { ImageFill } from "react-bootstrap-icons";
 
 const CreatePost = ({ onPostsUpdate, handleClose }) => {
-  const initialFormData = {
-    content: "",
-    image: null,
-    privacyType: 0,
-    selectedReceivers: [],
-  };
-
-  const [formData, setFormData] = useState(initialFormData);
-  const [followers, setFollowers] = useState([]);
-  const [errMsg, setErrMsg] = useState("");
-
-  const handleImageUpload = (image) => {
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      image: image,
-    }));
-  };
-  console.log(formData);
-  // errordata state
-
-  const handleChange = (event) => {
-    const { name, value, type } = event.target;
-
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      [name]: type === "radio" ? parseInt(value) : value,
-    }));
-  };
-
-  useEffect(() => {
-    const fetchFollowers = async () => {
-      try {
-        const response = await axios.get(FOLLOWERS_URL, {
-          withCredentials: true,
-        });
-        setFollowers(response.data);
-      } catch (err) {
-        console.error(err);
-      }
+    const initialFormData = {
+        content: "",
+        image: null,
+        privacyType: 1,
+        selectedReceivers: [],
     };
-    if (formData.privacyType === 3) {
-      fetchFollowers();
-    }
-  }, [formData.privacyType]);
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+    const [formData, setFormData] = useState(initialFormData);
+    const [followers, setFollowers] = useState([]);
+    const [errMsg, setErrMsg] = useState("");
 
-    if (
-      formData?.privacyType === 0 ||
-      (formData?.content === "" && formData?.image === null)
-    ) {
-      return;
-    }
+    const handleImageUpload = (image) => {
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            image: image,
+        }));
+    };
+    console.log(formData);
+    // errordata state
 
-    const formDataWithImage = new FormData();
-    formDataWithImage.append("content", formData.content);
-    formDataWithImage.append("privacyType", formData.privacyType);
-    formDataWithImage.append(
-      "selectedReceivers",
-      JSON.stringify(formData.selectedReceivers)
-    );
+    const handleChange = (event) => {
+        const { name, value, type } = event.target;
 
-    if (formData?.image) {
-      formDataWithImage.append("image", formData?.image); // Append the image file if it exists
-    }
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            [name]: type === "radio" ? parseInt(value) : value,
+        }));
+    };
 
-    console.log("data when submitted", formDataWithImage);
-
-    try {
-      const response = await axios.post(
-        "http://localhost:8000/post",
-        formDataWithImage,
-        // JSON.stringify(formData),
-        {
-          withCredentials: true,
-          headers: { "Content-Type": "multipart/form-data" },
+    useEffect(() => {
+        const fetchFollowers = async () => {
+            try {
+                const response = await axios.get(FOLLOWERS_URL, {
+                    withCredentials: true,
+                });
+                setFollowers(response.data);
+            } catch (err) {
+                console.error(err);
+            }
+        };
+        if (formData.privacyType === 3) {
+            fetchFollowers();
         }
-      );
+    }, [formData.privacyType]);
 
-      setErrMsg(response.data?.message);
+    const handleSubmit = async (event) => {
+        event.preventDefault();
 
-      onPostsUpdate();
-      handleClose();
-    } catch (err) {
-      if (!err?.response) {
-        setErrMsg("No Server Response");
-      } else {
-        setErrMsg("Internal Server Error");
-      }
-    }
+        if (
+            formData?.privacyType === 0 ||
+            (formData?.content === "" && formData?.image === null)
+        ) {
+            return;
+        }
 
-    setFormData(initialFormData);
-  };
+        const formDataWithImage = new FormData();
+        formDataWithImage.append("content", formData.content);
+        formDataWithImage.append("privacyType", formData.privacyType);
+        formDataWithImage.append(
+            "selectedReceivers",
+            formData.selectedReceivers
+        );
 
-  const handleSelectChange = (selectedOptions) => {
-    const selectedValues = selectedOptions.map((option) =>
-      option.value.toString()
-    );
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      selectedReceivers: selectedValues,
+        if (formData?.image) {
+            formDataWithImage.append("image", formData?.image); // Append the image file if it exists
+        }
+
+        console.log("data when submitted", formDataWithImage);
+
+        try {
+            const response = await axios.post(
+                "http://localhost:8000/post",
+                formDataWithImage,
+                // JSON.stringify(formData),
+                {
+                    withCredentials: true,
+                    headers: { "Content-Type": "multipart/form-data" },
+                }
+            );
+
+            setErrMsg(response.data?.message);
+
+            onPostsUpdate();
+            handleClose();
+        } catch (err) {
+            if (!err?.response) {
+                setErrMsg("No Server Response");
+            } else {
+                setErrMsg("Internal Server Error");
+            }
+        }
+
+        setFormData(initialFormData);
+    };
+
+    const handleSelectChange = (selectedOptions) => {
+        const selectedValues = selectedOptions.map((option) =>
+            option.value.toString()
+        );
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            selectedReceivers: selectedValues,
+        }));
+    };
+
+    const followersOptions = followers.map((follower) => ({
+        value: follower.id,
+        label: `${follower.firstName} ${follower.lastName}`,
     }));
-  };
 
-  const followersOptions = followers.map((follower) => ({
-    value: follower.id,
-    label: `${follower.firstName} ${follower.lastName}`,
-  }));
+    return (
+        <>
+            {errMsg && (
+                <Alert variant="danger" className="text-center">
+                    {errMsg}
+                </Alert>
+            )}
 
-  return (
-    <>
-      {errMsg && (
-        <Alert variant="danger" className="text-center">
-          {errMsg}
-        </Alert>
-      )}
+            {formData?.image && (
+                <div className="post-img mb-3">
+                    <Image
+                        src={URL.createObjectURL(formData?.image)}
+                        fluid
+                        alt="Selected"
+                    />
+                </div>
+            )}
 
-      {formData?.image && (
-        <div className="post-img mb-3">
-          <Image
-            src={URL.createObjectURL(formData?.image)}
-            fluid
-            alt="Selected"
-          />
-        </div>
-      )}
+            <Form onSubmit={handleSubmit}>
+                <Stack direction="horizontal">
+                    <Col>
+                        <Stack direction="horizontal">
+                            <InputGroup className="me-2">
+                                <Form.Control
+                                    as="textarea"
+                                    placeholder="Write what's on your mind"
+                                    onChange={handleChange}
+                                    value={formData.content}
+                                    name="content"
+                                />
+                            </InputGroup>
+                            <div>
+                                <GenericModal
+                                    variant="flush"
+                                    img={<ImageFill />}
+                                    buttonText="Add an image"
+                                >
+                                    <ImageUploadModal
+                                        onUploadSuccess={handleImageUpload}
+                                    />
+                                </GenericModal>
+                            </div>
+                            <div>
+                                <Button type="submit">Post</Button>
+                            </div>
+                        </Stack>
 
-      <Form onSubmit={handleSubmit}>
-        <Stack direction="horizontal">
-          <Col>
-            <Stack direction="horizontal">
-              <InputGroup className="me-2">
-                <Form.Control
-                  as="textarea"
-                  placeholder="Write what's on your mind"
-                  onChange={handleChange}
-                  value={formData.content}
-                  name="content"
-                />
-              </InputGroup>
-              <div>
-                <GenericModal
-                  variant="flush"
-                  img={<ImageFill />}
-                  buttonText="Add an image"
-                >
-                  <ImageUploadModal onUploadSuccess={handleImageUpload} />
-                </GenericModal>
-              </div>
-              <div>
-                <Button type="submit">Post</Button>
-              </div>
-            </Stack>
-
-            <Col className="mb-3">
-              <Form.Check
-                inline
-                label="Public"
-                name="privacyType"
-                type="radio"
-                id="public"
-                value={1}
-                onChange={handleChange}
-              />
-              <Form.Check
-                inline
-                label="Private"
-                name="privacyType"
-                type="radio"
-                id="private"
-                value={2}
-                onChange={handleChange}
-              />
-              <Form.Check
-                inline
-                label="Sub-private"
-                name="privacyType"
-                type="radio"
-                id="subPrivate"
-                value={3}
-                onChange={handleChange}
-              />
-            </Col>
-            <Col>
-              {formData.privacyType === 3 && (
-                <Select
-                  options={followersOptions}
-                  isMulti
-                  onChange={handleSelectChange}
-                />
-              )}
-            </Col>
-          </Col>
-        </Stack>
-      </Form>
-    </>
-  );
+                        <Col className="mb-3">
+                            <Form.Check
+                                inline
+                                label="Public"
+                                name="privacyType"
+                                type="radio"
+                                id="public"
+                                value={1}
+                                onChange={handleChange}
+                            />
+                            <Form.Check
+                                inline
+                                label="Private"
+                                name="privacyType"
+                                type="radio"
+                                id="private"
+                                value={2}
+                                onChange={handleChange}
+                            />
+                            <Form.Check
+                                inline
+                                label="Sub-private"
+                                name="privacyType"
+                                type="radio"
+                                id="subPrivate"
+                                value={3}
+                                onChange={handleChange}
+                            />
+                        </Col>
+                        <Col>
+                            {formData.privacyType === 3 && (
+                                <Select
+                                    options={followersOptions}
+                                    isMulti
+                                    onChange={handleSelectChange}
+                                />
+                            )}
+                        </Col>
+                    </Col>
+                </Stack>
+            </Form>
+        </>
+    );
 };
 
 export default CreatePost;
